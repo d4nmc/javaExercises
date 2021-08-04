fc7910347730c9aa016aa920354be21433016dae
d4nmc <danielmcgee.work@gmail.com>
2021-08-04 11:31:38 +0100

    MC: Working on the arrayExercise trying to create a square number generator / loop

---
+++ src/main/java/arrayPlay/ArrayExercise.java
@@ -0,0 +1,21 @@
+package arrayPlay;
+
+public class ArrayExercise {
+
+	public static void squareArray() {
+		int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int[] squareArray = { 
+		int newNum;
+
+		for (int i = 0; i < numberArray.length; i++) {
+			newNum = numberArray[i];
+			System.out.println(newNum * newNum);
+		
+		
+		int squareNum = numberArray[i]*numberArray[i];
+		
+	}
+}
+//  Create an array which takes in 10 numbers and lists them on print out
+//  Take each of those numbers entered onto array and produce a further array storing the square
+//  
